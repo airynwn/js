596f4c53809e50cc3abce867bca84cb0f61abd15
airynwn <laura.alcon@iesdonana.org>
2023-02-01 17:11:19 +0100

    tema 6 ej 2-5 (5 en proceso

---
+++ Tarea/U6/a05-u6-laura.js
@@ -0,0 +1,14 @@
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+
+p1.addEventListener("click", () => {
+    switch (p1.value) {
+        case "Ocultar": 
+            p1.
+            break;
+        case "Visualizar":
+        
+            break;
+    }
+})
