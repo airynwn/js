3900be98797b764d99c7937704f9b057b97dd8ba
axrynz <laura.alcon@iesdonana.org>
2023-02-09 16:59:32 +0100

    Qiuita espacios act2

--- Tarea/U7/a02-u7-laura.js
+++ Tarea/U7/a02-u7-laura.js
@@ -1,21 +1,20 @@
+let ciudad = document.getElementById("ciudad");
 
-        let ciudad = document.getElementById("ciudad");
-    
-        ciudad.addEventListener("keyup", mostrarSugerencia);
-    
-        function mostrarSugerencia() {
-            let inputCiudad = ciudad.value;
-            if (inputCiudad.length == 0) {
-                document.getElementById("sugerencia").innerHTML = "";
-                return;
-            } else {
-                let xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("sugerencia").innerHTML = this.responseText;
-                }
-            };
-                xmlhttp.open("GET", "ciudadessugerencias.php?q=" + inputCiudad, true);
-                xmlhttp.send();
-            }
+ciudad.addEventListener("keyup", mostrarSugerencia);
+
+function mostrarSugerencia() {
+    let inputCiudad = ciudad.value;
+    if (inputCiudad.length == 0) {
+        document.getElementById("sugerencia").innerHTML = "";
+        return;
+    } else {
+        let xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("sugerencia").innerHTML = this.responseText;
         }
+    };
+        xmlhttp.open("GET", "ciudadessugerencias.php?q=" + inputCiudad, true);
+        xmlhttp.send();
+    }
+}
