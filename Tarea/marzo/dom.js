ecedc8c724a34d23d6701074d3e34ffdf0bbc779
axrynz <laura.alcon@iesdonana.org>
2023-02-28 01:02:03 +0100

    dm

---
+++ Tarea/marzo/dom.js
@@ -0,0 +1,36 @@
+/*
+<ul>
+    <li>nodos
+        <ul>
+            <li>nodo1</li>
+            <li>nodo2</li>
+        </ul>
+        </li>
+        </ul>
+        */
+let res = document.getElementById("resultado");
+
+function generaListaRecursiva(nodo, lista) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(nodo.tagName));
+
+    if (nodo.children.length > 0) {
+        const ul = document.createElement("ul");
+
+        for (c of nodo.children) {
+            generaListaRecursiva(c, ul);
+        }
+
+        li.appendChild(ul);
+    }
+
+    lista.appendChild(li);
+}
+
+function generaLista() {
+    const ul = document.createElement("ul");
+    res.appendChild(ul);
+    generaListaRecursiva(document.documentElement, ul);
+}
+
+window.onload = generaLista;
